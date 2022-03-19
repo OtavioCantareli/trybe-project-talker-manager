a1d63d132c9360880bdadb9e50260804a736e7cf
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-03-19 11:31:45 -0300

    Req 4: age validation

--- index.js
+++ index.js
@@ -121,4 +121,20 @@ function validateName(request, response, next) {
   next();
 }
 
+function validateAge(request, response, next) {
+  const AGE = 18;
+  const { age } = request.body;
+  if (!age || age === '') {
+    return response.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < AGE) {
+    return response.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+  next();
+}
+
 
