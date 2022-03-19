cc9cfc61ec95f488083c16488b808763e0f7721c
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-03-19 11:32:32 -0300

    Req 4: talk validation

--- index.js
+++ index.js
@@ -155,4 +155,22 @@ function validateDateAndRate(request, response, next) {
   next();
 }
 
+function validateTalk(request, response, next) {
+  const { talk } = request.body;
+  if (!talk || talk === '') {
+    return response.status(400).json({
+      message:
+        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  const { watchedAt, rate } = talk;
+  if (!watchedAt || !rate) {
+    return response.status(400).json({
+      message:
+        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  next();
+}
+
 
